da2f594bf8058327415bdeaf264e6fad68198559
Rolls47 <quentin.adadain@gmail.com>
2020-05-13 22:22:26 +0200

    Symfony 06- Doctrine

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -13,7 +13,7 @@ Class WildController extends AbstractController
     /**
      * @Route("/wild", name="wild_index")
      */
-    public function index() :Response
+    public function index() : Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
