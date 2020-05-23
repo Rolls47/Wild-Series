858ca27d8b50ec1755636199dcc7cbc97322cf9c
Rolls47 <quentin.adadain@gmail.com>
2020-05-23 15:49:06 +0200

    symfony 11 - FORM

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Form\CategoryType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @route("/wild/category/add", name="category_add")
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function add(Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(CategoryType::class);
+        $form->handleRequest($request);
+    if ($form->isSubmitted()){
+        $category = $form->getData();
+
+        $manager->persist($category);
+        $manager->flush();
+        return $this->redirectToRoute('category_add');
+    }
+       return $this->render('category/add.html.twig', [
+            'form' => $form->createView()]);
+    }
+}
