d6bf8da26fb6ba2e0f2d0099c19ab0134a20386f
Rolls47 <quentin.adadain@gmail.com>
2020-05-28 22:08:13 +0200

    symfony 12 - VALIDATION

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,10 +6,14 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @ORM\Entity(repositoryClass="App\Repository\ProgramRepository", repositoryClass=ProgramRepository::class)
+ * @UniqueEntity("title", message="ce program existe déjà")
  */
+
 class Program
 {
     /**
@@ -20,17 +24,28 @@ class Program
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="le champ ne doit pas être vide")
+     * @Assert\Length(max="255", maxMessage="le titre doit faire moins de 255 caractère", allowEmptyString = false)
      */
     private $title;
 
     /**
+     * @Assert\Regex(
+     *     pattern="/plus belle la vie/m",
+     *     match=false,
+     *     message="on parle de vrai serie ici"
+     * )
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="le champ ne doit pas être vide")
+     * @Assert\Length(max="255", maxMessage="le résumé doit faire moins de 255 caractère")
      */
     private $summary;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank(message="le champ ne doit pas être vide")
+     * @Assert\Length(max="255", maxMessage="le lien du poster doit faire moins de 255 caractère")
      */
     private $poster;
 
